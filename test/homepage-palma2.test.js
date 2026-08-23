1fe87b23de9dd11e2260d8dc07e84167157b3a6d
Wulu <invincible9257@gmail.com>
2026-08-23 11:33:56 +0800

    test: expect generic ink screen mode label

--- test/homepage-palma2.test.js
+++ test/homepage-palma2.test.js
@@ -4,8 +4,10 @@ import { readFileSync } from 'node:fs';
 
 const template = readFileSync(new URL('../src/index.njk', import.meta.url), 'utf8');
 
-test('latest digest homepage renders the Palma 2 mode control', () => {
+test('latest digest homepage renders the ink screen mode control', () => {
   assert.match(template, /data-palma-toggle/);
+  assert.match(template, />墨水屏模式</);
+  assert.doesNotMatch(template, />Palma 2 模式</);
 });
 
 test('latest digest homepage exposes previous-issue tap navigation when available', () => {
